3406829f5b739a6a8331e1e69090f6b4a5225c12
Sasha Bezrodnov <garri348@gmail.com>
2026-02-19 22:33:41 +0500

    added tests: checkelementsontopbar, checkmidelements, signup update build.gradle

--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -1,13 +1,49 @@
+import com.codeborne.selenide.Configuration;
+import net.bytebuddy.build.Plugin;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTests {
+
+  @BeforeAll
+  static void beforeAll() {
+      Configuration.browser = "chrome";
+      Configuration.browserSize = "1920x1080";
+      Configuration.pageLoadStrategy = "eager";
+      open("https://www.hltv.org/");
+  }
+
    @Test
-    void just() {
-        open("https://www.hltv.org/");
-        $(".nav-link ").shouldHave(text("News"));
+    void CheckElementsOnTopBar() {
+       $("#navBarContainerFull").shouldHave(text("News"));
+       $("#navBarContainerFull").shouldHave(text("Matches"));
+       $("#navBarContainerFull").shouldHave(text("Results"));
+       $("#navBarContainerFull").shouldHave(text("Events"));
+       $("#navBarContainerFull").shouldHave(text("Players"));
+       $("#navBarContainerFull").shouldHave(text("Stats"));
+       $("#navBarContainerFull").shouldHave(text("Fantasy"));
+       $("#navBarContainerFull").shouldHave(text("Forum"));
+       $("#navBarContainerFull").shouldHave(text("Media"));
+       $("#navBarContainerFull").shouldHave(text("Betting"));
+       $("#navBarContainerFull").shouldHave(text("Live"));
+    }
+
+    @Test
+    void CheckMidElements() {
+      $(".colCon").shouldHave(text("Overview"));
+        $(".colCon").shouldHave(text("Matches"));
+        $(".colCon").shouldHave(text("Results"));
+        $(".colCon").shouldHave(text("Stats"));
+        $(".colCon").shouldHave(text("Yesterday's news"));
+        $(".colCon").shouldHave(text("Today's news"));
+
+    }
+
+    @Test
+    void SignUp() {
+      $(".navsearchinput tt-input").setValue("ZyWoo");
     }
 }
