9071c42eb648a72767c2815a87bb20addb0ed51c
Sasha Bezrodnov <garri348@gmail.com>
2026-02-23 21:34:18 +0500

    added Attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,7 +18,23 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-         Configuration.pageLoadStrategy = "eager";
+        Configuration.pageLoadStrategy = "eager";
         open("https://www.hltv.org/");
+    }
+
+    @BeforeEach
+        //метод выполняется перед каждым тестом
+    void setUpBeforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()); //включение слушателя Аллюр
+    }
+
+    @AfterEach
+        //выполняется после каждого теста
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot"); //Скриншот последнего состояния браузера.
+        Attach.pageSource(); //Исходный код страницы.
+        Attach.browserConsoleLogs(); //Логи консоли браузера.
+        Attach.addVideo(); //Видео записи теста.
+        Selenide.closeWebDriver(); //Закрывает браузер.
     }
 }
